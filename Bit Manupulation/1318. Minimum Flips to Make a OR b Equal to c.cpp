0752187c4a08bd924660cce5780b4073f68742c7
Praizy <121686362+22-07-03@users.noreply.github.com>
2024-06-06 01:53:15 +0530

    1318. Minimum Flips to Make a OR b Equal to c.cpp

---
+++ Bit Manupulation/1318. Minimum Flips to Make a OR b Equal to c.cpp
@@ -0,0 +1,29 @@
+#pragma GCC optimize("03")
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        int ans = 0;
+
+        while(a || b || c){
+            int bitA = a&1;
+            int bitB = b&1;
+            int bitC = c&1;
+
+            if(bitC == 0){
+                if(bitA==1 && bitB==1) ans+=2;
+                else if(bitA || bitB) ans+= 1;
+            }
+
+            else if(bitC ==1 && bitA==0 && bitB==0){
+                ans+= 1;
+            }
+
+            a>>= 1;
+            b>>= 1;
+            c>>= 1;
+        }
+
+        return ans;
+    }
+};
